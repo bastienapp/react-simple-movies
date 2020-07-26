2d8785fe9c732aa8c8132836e6cc2a56dc58e3e9
bastienwcs <bastien@wldcodeschool.fr>
2020-07-26 10:18:47 +0200

    init project

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Movie from "models/Movie";
+import "./MovieCard.css";
+
+const MovieCard = (props) => {
+  return (
+    <div className="MovieCard">
+      <ul>
+        <li>
+          <a
+            href={"https://www.imdb.com/title/" + props.movie.id + "/"}
+            target="_blank"
+          >
+            {props.movie.title}
+          </a>
+        </li>
+        <li>
+          <img src={props.movie.poster} alt={props.movie.title} />
+        </li>
+        <li>{props.movie.director}</li>
+        <li>{props.movie.year}</li>
+      </ul>
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.instanceOf(Movie),
+};
+
+export default MovieCard;
---
+++ src/models/Movie.js
@@ -0,0 +1,13 @@
+class Movie {
+
+    constructor(id, title, director, year, rating, poster) {
+        this.id = id;
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.rating = rating;
+        this.poster = poster;
+    }
+}
+
+export default Movie;
