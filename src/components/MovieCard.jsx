7fb46dab72c91cde4325f1d4349331fd7080dec2
bastienwcs <bastien@wldcodeschool.fr>
2020-07-31 18:49:46 +0200

    save favourite in the state

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,29 +3,51 @@ import PropTypes from 'prop-types';
 import Movie from './Movie';
 import './MovieCard.css';
 
-const MovieCard = (props) => {
-  const { movie } = props;
-  return (
-    <div className="MovieCard">
-      <ul>
-        <li>
-          <a
-            href={`https://www.imdb.com/title/${movie.id}/`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {movie.title}
-          </a>
-        </li>
-        <li>
-          <img src={movie.poster} alt={movie.title} />
-        </li>
-        <li>{movie.director}</li>
-        <li>{movie.year}</li>
-      </ul>
-    </div>
-  );
-};
+class MovieCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favourite: false,
+    };
+  }
+
+  render() {
+    const { movie } = this.props;
+    const { favourite } = this.state;
+    return (
+      <div className="MovieCard">
+        <ul>
+          <li>
+            <a
+              href={`https://www.imdb.com/title/${movie.id}/`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {movie.title}
+            </a>
+          </li>
+          <li>
+            <img src={movie.poster} alt={movie.title} />
+          </li>
+          <li>{movie.director}</li>
+          <li>{movie.year}</li>
+          <li>
+            <button
+              type="button"
+              onClick={() => {
+                this.setState({
+                  favourite: !favourite,
+                });
+              }}
+            >
+              {favourite ? 'Remove from favourite' : 'Add to favourite'}
+            </button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 MovieCard.propTypes = {
   movie: PropTypes.instanceOf(Movie).isRequired,
