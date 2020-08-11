1152c8eca491f046a5a23bf3fde275b1cde78691
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-08-11 17:03:54 +0200

    add movie filter with controlled input

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import MovieApi from './MovieApi';
 import MovieCard from './MovieCard';
+import Movie from './Movie';
 
 const MovieList = () => {
   const [error, setError] = useState(false);
   const [movies, setMovies] = useState([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     let cancelled = false;
@@ -28,11 +30,35 @@ const MovieList = () => {
     };
   }, []);
 
+  const filterMovies = (event) => {
+    const { target } = event;
+    const { value } = target;
+    setFilter(value);
+  };
+
   return (
     <div className="MovieList">
       {!error || <div>Error</div>}
+      {movies.length === 0 || (
+        <label htmlFor="filter-movies">
+          <span>Search a movie: </span>
+          <input
+            id="filter-movies"
+            name="filter"
+            onChange={(event) => filterMovies(event)}
+            value={filter}
+          />
+        </label>
+      )}
       {movies.length === 0 ||
-        movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        movies
+          .filter(
+            (movie) =>
+              !filter.length ||
+              (movie instanceof Movie &&
+                movie.title.toLowerCase().includes(filter.toLowerCase()))
+          )
+          .map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       {movies.length !== 0 || error || <div>Loading...</div>}
     </div>
   );
